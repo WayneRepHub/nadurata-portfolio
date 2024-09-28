73e2c7ccd32516f41b3b816c87ffd6b7f079133c
Wayne Nadurata <wgn0914@gmail.com>
2024-09-28 13:04:07 +0800

    lol

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-    <Routes>
+    <Routes basename="/nadurata-portfolio">
       <Route path ="/" element={<Home />} />
     </Routes>
     </>
